463df912e0ddd7e25608056fb51d948a00a6c099
ogutkowska <o.gutkowskaz@gmail.com>
2017-04-08 15:35:05 +0200

    define routes and controllers

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 'use strict' ;
+
+var weatherApp = angular.module('weatherApp', ['ngRoute']);
+
+weatherApp.config(['$routeProvider', function ($routeProvider){
+  $routeProvider
+  .when('/', {
+    templateUrl: '/views/home.html',
+    controller: 'homeController'
+  })
+  .when('/forecast', {
+    templateUrl: 'views/forecast.html',
+    controller: 'forecastController'
+  })
+}]);
+
+weatherApp.controller('homeController',['$scope', function ($scope) {
+  console.log('homeController');
+}]);
+
+weatherApp.controller('forecastController',['$scope', function ($scope) {
+  console.log('Forecast Controller');
+}]);
